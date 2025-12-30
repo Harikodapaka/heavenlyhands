79f7a271e534a4347b003f941ce81b742dd96cd9
Hari Chaitanya <24608554+Harikodapaka@users.noreply.github.com>
2025-12-30 17:45:58 -0500

    init commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import '@mantine/core/styles.css';
+
+import React from 'react';
+import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
+import { theme } from '../theme';
+
+export const metadata = {
+  title: 'Heavenly Hands - Massage Therapy in Cambridge, Kitchener & Waterloo',
+  description: 'The best RMT massage therapy clinic in Cambridge, Kitchener-Waterloo & Guelph. Heavenly Hands offers professional massage services to relieve stress, reduce pain, and improve overall health. Book your appointment now!',
+};
+
+export default function RootLayout({ children }: { children: any }) {
+  return (
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
---
+++ theme.ts
@@ -0,0 +1,58 @@
+'use client';
+
+import { createTheme } from '@mantine/core';
+
+export const theme = createTheme({
+  fontFamily: 'Montserrat, Helvetica, Arial, Lucida, sans-serif',
+  primaryColor: 'themeColor1',
+  colors: {
+    themeColor1: [
+      '#00b8b8',
+      '#00b8b8',
+      '#00b8b8',
+      '#00b8b8',
+      '#008080',
+      '#008080',
+      '#008080',
+      '#004848',
+      '#004848',
+      '#004848',
+    ],
+    themeColor2: [
+      '#005c4d',
+      '#005c4d',
+      '#005c4d',
+      '#005c4d',
+      '#00241e',
+      '#00241e',
+      '#00241e',
+      '#000000',
+      '#000000',
+      '#000000',
+    ],
+    themeColor3: [
+      '#a4fcfc',
+      '#a4fcfc',
+      '#a4fcfc',
+      '#a4fcfc',
+      '#6dfbfb',
+      '#6dfbfb',
+      '#6dfbfb',
+      '#36fafa',
+      '#36fafa',
+      '#36fafa',
+    ],
+    themeColor4: [
+      '#e5b163',
+      '#e5b163',
+      '#e5b163',
+      '#e5b163',
+      '#dd9933',
+      '#dd9933',
+      '#dd9933',
+      '#b97b1f',
+      '#b97b1f',
+      '#b97b1f',
+    ],
+  },
+});
