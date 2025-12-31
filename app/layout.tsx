4e2b38e32209bb365f0b4ebecc087bd13f185bcd
Hari Chaitanya <24608554+Harikodapaka@users.noreply.github.com>
2025-12-30 20:53:34 -0500

    create header and footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
 import '@mantine/core/styles.css';
 
 import React from 'react';
-import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
-import { theme } from '../theme';
+import logoImage from '@/public/heavenly-hands-logo.png';
+import { ColorSchemeScript, Container, mantineHtmlProps, MantineProvider } from '@mantine/core';
+import { Footer } from '@/components/Footer/Footer';
+import { Header } from '@/components/Header/Header';
+import { links } from '@/components/Header/links';
+import { theme } from '@/theme';
 
 export const metadata = {
   title: 'Heavenly Hands - Massage Therapy in Cambridge, Kitchener & Waterloo',
   description: 'The best RMT massage therapy clinic in Cambridge, Kitchener-Waterloo & Guelph. Heavenly Hands offers professional massage services to relieve stress, reduce pain, and improve overall health. Book your appointment now!',
 };
+
 
 export default function RootLayout({ children }: { children: any }) {
   return (
@@ -20,14 +25,26 @@ export default function RootLayout({ children }: { children: any }) {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
       </head>
       <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <MantineProvider theme={theme}>
+          <Header
+            links={links}
+            cta={{ label: 'Book Appointment', href: '#book' }}
+            logo={{ src: logoImage, alt: 'Heavenly Hands', href: '/' }}
+          />
+          <main>
+            <Container size="lg">
+              {children}
+            </Container>
+          </main>
+          <Footer />
+        </MantineProvider>
       </body>
     </html>
   );
